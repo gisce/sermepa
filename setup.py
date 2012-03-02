3163f2a37a99dcf987ea92e3647ab3acd79f8fe2
Marcos de Vera Piquero <mdevera@gisce.net>
2012-03-02 11:11:23 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup file for sermepa client.
+"""
+PACKAGES = ['sermepa', ]
+PACKAGES_DATA = {}
+
+setup(name='sermepa',
+      description = """A client to submit payment orders to the Sermepa
+      service.""",
+      author='GISCE Enginyeria',
+      author_email='devel@gisce.net',
+      url='http://www.gisce.net',
+      version='0.1.0-dev',
+      license='General Public Licence 2',
+      long_description='''Long description''',
+      provides=['sermepa'],
+      requires=[],
+      packages=PACKAGES,
+      package_data=PACKAGES_DATA,
+      scripts=[],
+)
