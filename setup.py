a949877fa5d0ec59a7e44e1317cf922204562a5e
David García Garzón <voki@canvoki.net>
2016-01-14 15:58:43 +0100

    setup.py: dependencies and descriptions

--- setup.py
+++ setup.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Setup file for sermepa client.
+"""\
+A client to submit payment orders to the Sermepa service.
 """
-
 from setuptools import setup
+
+with open("README.md") as readme:
+    longdesc = readme.read()
 
 PACKAGES = ['sermepa', ]
 PACKAGES_DATA = {}
 
-setup(name='sermepa',
-      description = """A client to submit payment orders to the Sermepa
-      service.""",
-      author='GISCE Enginyeria',
-      author_email='devel@gisce.net',
-      url='http://www.gisce.net',
-      version='0.1.1-dev',
-      license='General Public Licence 2',
-      long_description='''Long description''',
-      provides=['sermepa'],
-      test_suite='sermepa',
-      install_requires=[
+setup(
+    name='sermepa',
+    version='0.1.1-dev',
+    description = __doc__,
+    author='GISCE Enginyeria',
+    author_email='devel@gisce.net',
+    url='http://www.gisce.net',
+    license='General Public Licence 2',
+    long_description='''Long description''',
+    provides=['sermepa'],
+    test_suite='sermepa',
+    install_requires=[
         'pyDes',
+        'simplejson',
         ],
-      packages=PACKAGES,
-      package_data=PACKAGES_DATA,
-      scripts=[],
+    test_require=[
+        'requests',
+        ],
+    packages=PACKAGES,
+    package_data=PACKAGES_DATA,
+    scripts=[],
 )
